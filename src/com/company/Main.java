27c6b481af602cfb113f31c6cbd95de62943667a
Aishaouadah <aicha.ouadah@etu.usthb.dz>
2021-04-28 20:46:50 +0100

    generalize treatment file cnf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         File file_cnf = new File("test1.cnf");
-        int literrale =4;
-        addClause(file_cnf, literrale);
-        updateFile(file_cnf);
+        int literrale =3;
+        int n= updateFile(file_cnf);
+        System.out.println("n= "+n);
+        File file2_cnf = new File("test2.cnf");
+        addClause(file2_cnf, literrale,n);
         testerSat(file_cnf);
             }
         }
--- src/com/company/inference.java
+++ src/com/company/inference.java
@@ -6,48 +6,50 @@ import java.io.*;
    execute cmd
     */
 public class inference {
-    static void addClause(File f, int litteral) throws IOException {
+    static void addClause(File f, int litteral , int n) throws IOException {
        Writer file = new BufferedWriter(new FileWriter(f.getName(), true));
        file.write("\n");
+        if( litteral >= n){ System.out.println("erreur : ca depasse le nombre de variable"); System.exit(-1);}
         litteral=litteral*-1;
        String NonBut = String.valueOf(litteral);
        NonBut = NonBut+" 0";
-        file.append(NonBut);
-       file.close();
+       file.append(NonBut);
+        file.close();
     }
-    static void updateFile(File f) throws IOException {
-
+    static int updateFile(File f) throws IOException {
         File inputFile = new File("test1.cnf");
         File tempFile = new File("test2.cnf");
-
         BufferedReader reader = new BufferedReader(new FileReader(inputFile));
         Writer file = new BufferedWriter(new FileWriter("test2.cnf", true));
         //copy all expect toDelete
-        String toDelete ="p cnf 5 11";
-        file.write("p cnf 5 12\n");
-        String currentLine;
-
+        int nbr_var=0, nbr_clause = 0, i=1;String currentLine,toDelete=null;
         while((currentLine = reader.readLine()) != null) {
             // trim newline when comparing with lineToRemove
-            String trimmedLine = currentLine.trim(); System.out.println( "trimmedLine ="+trimmedLine);
+            String trimmedLine = currentLine.trim();
+            if(i ==1) {
+                 toDelete = currentLine; //System.out.println(" Todelete = "+toDelete);
+                String string = trimmedLine.substring(6);
+                System.out.println(" String =" + string);
+                nbr_var = Integer.valueOf( string.substring(0,1)); //System.out.println(" var = "+nbr_var);
+                nbr_clause = Integer.valueOf( string.substring(2));//System.out.println(" clauses = "+nbr_clause);
+                nbr_clause++;
+                file.append("p cnf ");
+                file.append(nbr_var+" ");
+                file.append(nbr_clause+"\n");
+            }//System.out.println( "trimmedLine ="+trimmedLine);
+            i++;
             if(trimmedLine.equals(toDelete)) continue;
             file.write(currentLine + System.getProperty("line.separator"));
-        }
-        file.close();
+        } file.close(); return nbr_var;
     }
     //tester le nv fichier
-    static void testerSat(File file) throws IOException {
+    static void testerSat(File file)  {
         try
-        {
-            String cmd ="ubcsat -alg saps -i test2.cnf -solve";
+        {   String cmd ="ubcsat -alg saps -i test2.cnf -solve";
             Runtime runtime = Runtime.getRuntime();
             Process p = runtime.exec(cmd);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(p.getErrorStream()));
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             // Read the output from the command
             System.out.println("Here is the standard output of the command:");
             String s;
@@ -55,15 +57,12 @@ public class inference {
                 if(s.equals("# No Solution found for -target 0")) System.out.println("NON SAT");
                 if(s.equals("Solution found for -target 0")) System.out.println("SAT");
             }
-
             System.out.println("Here is the standard error of the command (if any):\n");
             while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
+                System.out.println(s); }
         }
         catch(IOException e)
-        {
-            System.err.println("echec de l'execution du script: "+e);
+        {   System.err.println("echec de l'execution du script: "+e);
             System.exit(1);
         }
     }
